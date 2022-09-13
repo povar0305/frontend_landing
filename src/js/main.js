d8ea254fec431e344a1ed7500791dac53ec0b6dc
povar03052001 <soficko.sofi@yandex.ru>
2022-09-13 14:40:44 +0300

    done experience desc 1h

--- src/js/main.js
+++ src/js/main.js
@@ -7,6 +7,7 @@ import {actualYear} from './modules/actualYear';
 import header from './components/header';
 import lazyLoading from './modules/lazyLoading';
 import scrollToAnchor from './modules/scrollToAnchor';
+import {indexOf} from 'core-js/internals/array-includes';
 
 ieFix();
 vhFix();
@@ -16,9 +17,13 @@ scrollToAnchor.init();
 header.init();
 lazyLoading.init();
 
-$(document).ready(function (){
-	$('.header_burger').click(function (){
-		$('.header_burger img').toggle()
-		$('.header').toggleClass('open')
-	})
-})
+$(document)
+	.ready(function () {
+		$('.header_burger')
+			.click(function () {
+				$('.header_burger img')
+					.toggle();
+				$('.header')
+					.toggleClass('open');
+			});
+	});
